d1a51d8be75f5bf7fac0ba73daf379aa60bac716
apocalypse_araneta <apocalypse_araneta@yahoo.com>
2022-07-16 11:19:36 +0100

    corrected method name on unittest

--- application/controllers/Demotest.php
+++ application/controllers/Demotest.php
@@ -15,7 +15,6 @@ class Demotest extends Demo
 
     function testGetPeriod()
     {
-       
         $dt = new DateTime();
         $currentYear = $dt->format('Y');
         $months = 12;
@@ -34,14 +33,15 @@ class Demotest extends Demo
             'Nov/22',
             'Dec/22'
         ];
+
         for ($month = 1; $month <= $months; $month ++) {
             $i = $month - 1;
-            
+
             $maxDays = cal_days_in_month(CAL_GREGORIAN, $month, $currentYear);
             $dt->setDate($currentYear, $month, $maxDays);
 
-            $actual = $this->getPeriod($dt, $currentYear, $month);
-            
+            $actual = $this->getPeriod($dt);
+
             $this->unit->run($expected[$i], $actual, $actual);
         }
 
@@ -53,16 +53,17 @@ class Demotest extends Demo
         $dt = new DateTime();
         $currentYear = $dt->format('Y');
         $months = 12;
-        
+
         /**
          * Expected Payment Dates
+         *
          * @var array $expected
          */
         $expected = [
             '2022-01-31',
             '2022-02-28',
             '2022-03-31',
-            '2022-04-29',  // 30th is Saturday
+            '2022-04-29', // 30th is Saturday
             '2022-05-31',
             '2022-06-30',
             '2022-07-29', // 31 is Sunday
@@ -70,21 +71,21 @@ class Demotest extends Demo
             '2022-09-30',
             '2022-10-31',
             '2022-11-30',
-            '2022-12-30' //31 is Saurday
+            '2022-12-30' // 31 is Saurday
         ];
         $actual = $this->getBasicPaymentDates($dt);
-        
+
         for ($month = 1; $month <= $months; $month ++) {
             $i = $month - 1;
-            
+
             $maxDays = cal_days_in_month(CAL_GREGORIAN, $month, $currentYear);
             $dt->setDate($currentYear, $month, $maxDays);
-            
+
             $actual = $this->getBasicPaymentDates($dt);
-            
+
             $this->unit->run($expected[$i], $actual, $actual);
         }
-        
+
         print_r($this->unit->result());
     }
 
@@ -93,16 +94,17 @@ class Demotest extends Demo
         $dt = new DateTime();
         $currentYear = $dt->format('Y');
         $months = 12;
-        
+
         /**
          * Expected Payment Dates
+         *
          * @var array $expected
          */
         $expected = [
             '2022-01-10',
             '2022-02-10',
             '2022-03-10',
-            '2022-04-11',  // 10th is Sunday
+            '2022-04-11', // 10th is Sunday
             '2022-05-10',
             '2022-06-10',
             '2022-07-11', // 10th is Sunday
@@ -110,21 +112,21 @@ class Demotest extends Demo
             '2022-09-10',
             '2022-10-10',
             '2022-11-10',
-            '2022-12-12' //10th  is Saurday
+            '2022-12-12' // 10th is Saurday
         ];
         $actual = $this->getBasicPaymentDates($dt);
-        
+
         for ($month = 1; $month <= $months; $month ++) {
             $i = $month - 1;
-            
+
             $maxDays = cal_days_in_month(CAL_GREGORIAN, $month, $currentYear);
             $dt->setDate($currentYear, $month, $maxDays);
-            
+
             $actual = $this->getBonusDates($dt);
-            
+
             $this->unit->run($expected[$i], $actual, $actual);
         }
-        
+
         print_r($this->unit->result());
     }
 }
